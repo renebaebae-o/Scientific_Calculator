cfdaf4ba0e0d8f6c8699421e1e65f7097bc8b749
renebaebae-o <thereal.wangyufei04@gmail.com>
2025-02-17 19:29:41 +0800

    Update Calculator_function.py
    Added a function to calculate string expressions.Added checking of special domains of trigonometric functions.Added check for non-negativity of square root functions.

--- Calculator_function.py
+++ Calculator_function.py
@@ -1,4 +1,17 @@
+
 import math
+
+
+def evaluate_expression(expression):
+    # Calculating the result of a string expression
+    try:
+        # The eval function executes the incoming string expression.
+        result = eval(expression, {"__builtins__": None}, math.__dict__)
+        # Returns the result of the expression calculation
+        return result
+    except Exception as e:
+        # If any exception occurs, an error message is returned
+        return "Error: " + str(e)
 
 
 def compute_sin(angle):
@@ -35,6 +48,10 @@ def compute_arcsin(value):
     # Calculate the arcsine value
     try:
         value = float(value)
+        # Checks if the value is within the legal range. 
+        # The domain of definition of the sine is [-1, 1].
+        if value < -1 or value > 1:
+            return "Error: arcsin argument must be between -1 and 1"
         return math.degrees(math.asin(value))
     except Exception as e:
         return "Error: " + str(e)
@@ -43,10 +60,14 @@ def compute_arccos(value):
     # Calculating the inverse cosine
     try:
         value = float(value)
+        # Checks if the value is within the legal range. 
+        # The domain of definition of the s is [-1, 1].
+        if value < -1 or value > 1:
+            return "Error: arccos argument must be between -1 and 1"
         return math.degrees(math.acos(value))
     except Exception as e:
         return "Error: " + str(e)
-
+ 
 def compute_arctan(value):
     # Calculating the Arctangent
     try:
@@ -65,7 +86,12 @@ def compute_square(value):
 def compute_sqrt(value):
     # Calculate square root
     try:
+        value = float(value)
+        # Checks if the value is negative, 
+        # negative numbers do not have a real square root
+        if value < 0:
+            return "Error: sqrt is not defined for negative numbers"
         return math.sqrt(float(value))
     except Exception as e:
         return "Error: " + str(e)
-
+    
